18986eb8d60a7cc75fe4da90c7c2732b692c19b1
Amarjith S <amarjithappuuzz@gmail.com>
2026-02-20 16:29:22 +0530

    MY approach code for second largest

--- hello2.py
+++ hello2.py
@@ -1,6 +1,17 @@
-a=[12,13,14.15,16,"mrjith",34.5]
+l=[12,23,34,56,78,90,44]
+largest=l[0]
+index=0
+second_largest=l[0]
 
-for i in range (len(a)):
-    print(a[i])
+for i in range(len(l)):
+    if l[i]>largest:
+        largest=l[i]
+        index= i
+print(f"The largest number is {largest} and the index is {index} ")
 
-
+l.remove(largest)
+for i in range(len(l)):
+    if l[i]>second_largest:
+        second_largest=l[i]
+        index= i
+print(f"The second largest number is {second_largest } and the index is {index} ")
