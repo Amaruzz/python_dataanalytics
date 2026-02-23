9dfc4808e42a91a7da9d4552b0dd37fca8514231
Amarjith S <amarjithappuuzz@gmail.com>
2026-02-23 15:58:37 +0530

    shallow copy

--- hello2.py
+++ hello2.py
@@ -1,17 +1,5 @@
-l=[12,23,34,56,78,90,44]
-largest=l[0]
-index=0
-second_largest=l[0]
+a=[1,2,3,4,5,6,7]
 
-for i in range(len(l)):
-    if l[i]>largest:
-        largest=l[i]
-        index= i
-print(f"The largest number is {largest} and the index is {index} ")
-
-l.remove(largest)
-for i in range(len(l)):
-    if l[i]>second_largest:
-        second_largest=l[i]
-        index= i
-print(f"The second largest number is {second_largest } and the index is {index} ")
+b=a.copy() ##Shallow copy
+b[0]=100
+print(a)
