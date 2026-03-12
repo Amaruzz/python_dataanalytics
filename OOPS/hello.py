2b04052facf13ab62bbd488d7c9d49f9b87ef452
Amarjith S <amarjithappuuzz@gmail.com>
2026-03-12 15:12:56 +0530

    modules&packges

--- OOPS/hello.py
+++ OOPS/hello.py
@@ -1,14 +1,3 @@
-def decorate(func):
-    def wrapper(a,b):
-       print("the addition to u r number are")
-       func(a,b)
-       print("Thankyou I hope u liked it")
-    return wrapper
+import maths
 
- 
-
-@decorate
-def addition(a,b):
-    print(f" Your total is{a+b}")
-
-addition(12,67)
+print(maths.multiplication(12,12))
